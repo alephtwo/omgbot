fe6699c892d6c1994982755b4c53aca01bc60942
Ben Christianson <thhuntertgm@gmail.com>
2018-11-30 12:44:36 -0600

    Synchronize blocks

--- omg.rb
+++ omg.rb
@@ -1,19 +1,25 @@
 require 'discordrb'
+require 'thread'
 
-token = 'B0T.T0KEN.here'
+token = ARGV[0]
 bot = Discordrb::Commands::CommandBot.new token: token, prefix: '!'
-
 puts "This bot's invite URL is #{bot.invite_url}."
-puts 'Click on it to invite it to your server.'
 
+sounds = File.join __dir__, 'sounds', '*'
+
+voice_channel_lock = Mutex.new
 bot.command :omg do |event|
   channel = event.user.voice_channel
   return unless channel
 
-  # TODO: Block until after we've disconnected
-  voice_bot = bot.voice_connect channel
-  voice_bot.play_file File.join __DIR__, 'sounds', 'michiru.mp3'
-  voice_bot.destroy
+  chosen_sound = Dir.glob(sounds).sample
+
+  # Needs to be synchronized, otherwise we'll try to do too much at once.
+  voice_channel_lock.synchronize do
+    voice_bot = bot.voice_connect channel
+    voice_bot.play_file chosen_sound
+    voice_bot.destroy
+  end
   nil
 end
 

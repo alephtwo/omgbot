1810cf93c9de396257e0e1b5dc1e96d4c1de5b53
Ben Christianson <thhuntertgm@gmail.com>
2018-12-10 18:52:09 -0600

    Add michiru command

--- omg.rb
+++ omg.rb
@@ -5,14 +5,12 @@ token = ARGV[0]
 bot = Discordrb::Commands::CommandBot.new token: token, prefix: '!'
 puts "This bot's invite URL is #{bot.invite_url}."
 
-sounds = File.join __dir__, 'sounds', '*'
-
+sounds = File.join __dir__, 'sounds'
 voice_channel_lock = Mutex.new
-bot.command :omg do |event|
+
+def play_sound(event, file) do
   channel = event.user.voice_channel
   return unless channel
-
-  chosen_sound = Dir.glob(sounds).sample
 
   # Needs to be synchronized, otherwise we'll try to do too much at once.
   voice_channel_lock.synchronize do
@@ -22,5 +20,8 @@ bot.command :omg do |event|
   end
   nil
 end
+
+bot.command :omg { |e| play_sound(e, Dir.glob(File.join(sounds, '*')).sample) }
+bot.command :michiru { |e| play_sound(File.join(sounds, 'michiru.mp3')) }
 
 bot.run

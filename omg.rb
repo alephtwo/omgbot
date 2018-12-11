a2f89e3d2187a480becf9343e203377091965908
Ben Christianson <thhuntertgm@gmail.com>
2018-12-10 19:02:38 -0600

    Wow, it works

--- omg.rb
+++ omg.rb
@@ -1,5 +1,4 @@
 require 'discordrb'
-require 'thread'
 
 token = ARGV[0]
 bot = Discordrb::Commands::CommandBot.new token: token, prefix: '!'
@@ -8,20 +7,21 @@ puts "This bot's invite URL is #{bot.invite_url}."
 sounds = File.join __dir__, 'sounds'
 voice_channel_lock = Mutex.new
 
-def play_sound(event, file) do
-  channel = event.user.voice_channel
-  return unless channel
+play_sound = lambda do |sound|
+  lambda do |event|
+    channel = event.user.voice_channel
+    return unless channel
 
-  # Needs to be synchronized, otherwise we'll try to do too much at once.
-  voice_channel_lock.synchronize do
-    voice_bot = bot.voice_connect channel
-    voice_bot.play_file chosen_sound
-    voice_bot.destroy
+    # Needs to be synchronized, otherwise we'll try to do too much at once.
+    voice_channel_lock.synchronize do
+      voice_bot = bot.voice_connect channel
+      voice_bot.play_file sound
+      voice_bot.destroy
+    end
+    nil
   end
-  nil
 end
 
-bot.command :omg { |e| play_sound(e, Dir.glob(File.join(sounds, '*')).sample) }
-bot.command :michiru { |e| play_sound(File.join(sounds, 'michiru.mp3')) }
-
+bot.command :omg, &play_sound.call(Dir.glob(File.join(sounds, '*')).sample)
+bot.command :michiru, &play_sound.call(File.join(sounds, 'michiru.mp3'))
 bot.run

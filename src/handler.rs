964856b535992f71cf6976dc004af8b5c5f8240b
Ben Christianson <thhuntertgm@gmail.com>
2019-11-21 12:10:08 -0600

    Start rust

---
+++ src/handler.rs
@@ -0,0 +1,19 @@
+use serenity::prelude::{EventHandler, Context};
+use serenity::model::prelude::{Message, Ready};
+
+
+
+pub struct Handler;
+impl EventHandler for Handler {
+    fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!") {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+
+    fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}

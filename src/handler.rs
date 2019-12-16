fa929bd9cc772b157f614a71fb05f728deff8568
Ben Christianson <thhuntertgm@gmail.com>
2019-12-16 10:18:39 -0600

    Add invite url

--- src/handler.rs
+++ src/handler.rs
@@ -1,5 +1,9 @@
 use serenity::model::prelude::Message;
 use serenity::prelude::{Context, EventHandler};
+use serenity::model::gateway::Ready;
+use serenity::model::Permissions;
+use std::sync::Arc;
+use serenity::http::Http;
 
 pub struct Handler;
 
@@ -10,5 +14,18 @@ impl EventHandler for Handler {
                 println!("Error sending message: {:?}", why);
             }
         }
+    }
+
+    fn ready(&self, ctx: Context, payload: Ready) {
+        println!("[{}] connected to Discord.", payload.user.name);
+        let url = match payload.user.invite_url(&ctx.http, Permissions::empty()) {
+            Ok(v) => v,
+            Err(why) => {
+                eprintln!("Error getting invite url: {:?}", why);
+                return;
+            }
+        };
+
+        println!("Invite URL: {}", url);
     }
 }

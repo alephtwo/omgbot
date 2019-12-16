99f8b56c8e54d8661b20cc45e29f61aa97e3e125
Ben Christianson <thhuntertgm@gmail.com>
2019-12-16 10:08:32 -0600

    Restart handler

--- src/handler.rs
+++ src/handler.rs
@@ -1,19 +1,14 @@
-use serenity::prelude::{EventHandler, Context};
-use serenity::model::prelude::{Message, Ready};
-
-
+use serenity::model::prelude::Message;
+use serenity::prelude::{Context, EventHandler};
 
 pub struct Handler;
+
 impl EventHandler for Handler {
     fn message(&self, ctx: Context, msg: Message) {
         if msg.content == "!ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!") {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "pong!") {
                 println!("Error sending message: {:?}", why);
             }
         }
-    }
-
-    fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,15 @@
+use handler::Handler;
+use serenity::Client;
 use std::env;
 
-use serenity::Client;
-use serenity::framework::standard::CommandResult;
-use serenity::framework::standard::macros::command;
-use serenity::framework::standard::macros::group;
-use serenity::framework::StandardFramework;
-use serenity::model::channel::Message;
-use serenity::prelude::{Context, EventHandler};
+mod handler;
 
 fn main() {
-    let token = env::var("DISCORD_TOKEN")
-        .expect("DISCORD_TOKEN must be set.");
+    let token = env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN must be set.");
 
-    let mut client = Client::new(&token, Handler)
-        .expect("Error creating client.");
+    let mut client = Client::new(&token, Handler).expect("Error creating client.");
 
-    let framework = StandardFramework::new()
-        .configure(|c| c.prefix("!"))
-        .group(&GENERAL_GROUP);
-
-    client.with_framework(framework);
-
-    if let Err(why) = client.start () {
+    if let Err(why) = client.start() {
         println!("An error occurred while starting the client: {:?}", why);
     }
-}
-
-struct Handler;
-impl EventHandler for Handler {}
-
-group!({
-    name: "general",
-    options: {},
-    commands: [ping]
-});
-
-#[command]
-fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "pong").expect("unable to pong");
-    Ok(())
 }

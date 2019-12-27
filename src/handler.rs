05757b791aefc04bfcbb24c6a8174ff90268bb59
Ben Christianson <thhuntertgm@gmail.com>
2019-12-26 20:07:17 -0600

    Add file uploading when user not in channel

--- src/handler.rs
+++ src/handler.rs
@@ -9,7 +9,8 @@ use serenity::{
     },
     prelude::{Context, EventHandler, Mutex, RwLock, TypeMapKey},
 };
-use std::{sync::Arc, thread, time::Duration};
+use std::{sync::Arc, thread, time::Duration, path::PathBuf};
+use std::fs::File;
 
 pub struct Handler;
 pub struct VoiceManager;
@@ -35,14 +36,20 @@ impl EventHandler for Handler {
             }
         };
 
+        // Let's pick a file to play.
+        let source = match sound_picker::pick_file(command) {
+            Some(s) => s,
+            None => {
+                eprintln!("No sounds in path.");
+                return;
+            }
+        };
+
         // What voice channel is this user in?
         let channel = match get_voice_channel_for_user(&guild, &msg) {
             Some(c) => c,
             None => {
-                eprintln!(
-                    "User {} issued command {} but is not in a voice channel.",
-                    msg.author.name, command
-                );
+                upload_sound(&ctx, &msg, &source);
                 return;
             }
         };
@@ -56,10 +63,13 @@ impl EventHandler for Handler {
             }
         };
 
-        // Let's pick a file to play.
-        let source = match sound_picker::pick(command) {
+        // Convert the source to an AudioSource so it can be played.
+        let audio_source = match sound_picker::to_audio_source(&source) {
             Some(s) => s,
-            None => return,
+            None => {
+                eprintln!("Audio could not be converted to an AudioSource.");
+                return;
+            }
         };
 
         // We know the user's in a voice channel. Let's join it and play the sound...
@@ -72,7 +82,7 @@ impl EventHandler for Handler {
                 // The sound might play while the connected "ping" is still playing.
                 // Sleep for just a little bit before playing.
                 thread::sleep(Duration::from_secs(1));
-                handler.play_only(source)
+                handler.play_only(audio_source)
             }
             None => {
                 eprintln!("Unable to get a handler for the voice channel.");
@@ -123,4 +133,27 @@ fn get_voice_channel_for_user(guild: &Arc<RwLock<Guild>>, msg: &Message) -> Opti
 
 fn get_voice_manager_from_cache(ctx: &Context) -> Option<Arc<Mutex<ClientVoiceManager>>> {
     ctx.data.read().get::<VoiceManager>().cloned()
+}
+
+fn upload_sound(ctx: &Context, msg: &Message, source: &PathBuf) {
+    let file = match File::open(source) {
+        Ok(file) => file,
+        Err(why) => {
+            eprintln!("Error opening file: {:?}", why);
+            return;
+        }
+    };
+
+    let name = source.file_name().unwrap().to_str().unwrap();
+
+    let result = msg.channel_id.send_files(&ctx.http, vec![(&file, name)], |m| {
+        m.content("You aren't in a channel, but here's the sound...");
+        m
+    });
+    match result {
+        Ok(_a) => {},
+        Err(why) => {
+            eprintln!("Unable to upload sound: {:?}", why);
+        }
+    };
 }
--- src/sound_picker.rs
+++ src/sound_picker.rs
@@ -2,7 +2,7 @@ use rand::{seq::SliceRandom, thread_rng};
 use serenity::voice::{ffmpeg, AudioSource};
 use std::{env, fs, path::PathBuf};
 
-pub fn pick(category: String) -> Option<Box<dyn AudioSource>> {
+pub fn pick_file(category: String) -> Option<PathBuf> {
     let path = env::current_exe()
         .unwrap()
         .parent()
@@ -15,14 +15,15 @@ pub fn pick(category: String) -> Option<Box<dyn AudioSource>> {
         .map(|p| p.unwrap().path())
         .collect();
 
-    let path = match paths.choose(&mut thread_rng()) {
-        Some(p) => p,
-        None => {
-            eprintln!("No paths");
-            return None;
-        }
-    };
+    let result = paths.choose(&mut thread_rng());
 
+    match result {
+        Some(t) => Some(t.to_path_buf()),
+        None => None
+    }
+}
+
+pub fn to_audio_source(path: &PathBuf) -> Option<Box<dyn AudioSource>> {
     match ffmpeg(path) {
         Ok(source) => Some(source),
         Err(why) => {

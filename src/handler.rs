40345b4e4ae2f3b326912e81bc34c9a0d98344c9
Ben Christianson <thhuntertgm@gmail.com>
2020-01-02 11:05:38 -0600

    Use unwrap more liberally

--- src/handler.rs
+++ src/handler.rs
@@ -21,74 +21,44 @@ impl TypeMapKey for VoiceManager {
 
 impl EventHandler for Handler {
     fn message(&self, ctx: Context, msg: Message) {
-        // Only allow valid commands to get through.
-        let command = match commands::parse_command(&msg) {
-            Some(t) => t,
-            None => return,
-        };
+        let command = commands::parse_command(&msg).unwrap();
 
         // We will need to know what guild we're in.
-        let guild = match msg.guild(&ctx.cache) {
-            Some(guild) => guild,
-            None => {
-                eprintln!("Groups and DMs are not supported.");
-                return;
-            }
-        };
+        let guild = msg.guild(&ctx.cache).unwrap();
 
         // Let's pick a file to play.
-        let source = match sound_picker::pick_file(command) {
-            Some(s) => s,
-            None => {
-                eprintln!("No sounds in path.");
-                return;
-            }
-        };
+        let source = sound_picker::pick_file(command).unwrap();
 
         // What voice channel is this user in?
         let channel = match get_voice_channel_for_user(&guild, &msg) {
             Some(c) => c,
             None => {
+                // They're not in a channel, so we should upload the file instead.
                 upload_sound(&ctx, &msg, &source);
                 return;
             }
         };
 
         // Grab the voice manager from the cache.
-        let voice_manager = match get_voice_manager_from_cache(&ctx) {
-            Some(vm) => vm,
-            None => {
-                eprintln!("No voice manager in cache.");
-                return;
-            }
-        };
+        let voice_manager = get_voice_manager_from_cache(&ctx).unwrap();
 
         // Convert the source to an AudioSource so it can be played.
-        let audio_source = match voice::ffmpeg(&source).ok() {
-            Some(s) => s,
-            None => {
-                eprintln!("Audio could not be converted to an AudioSource.");
-                return;
-            }
-        };
+        let audio_source = voice::ffmpeg(&source).unwrap();
 
         // We know the user's in a voice channel. Let's join it and play the sound...
         let guild_id = guild.read().id;
         let mut manager = voice_manager.lock();
         // Get a handle to the audio.
-        let audio_lock = match manager.join(guild_id, channel) {
-            Some(handler) => {
+        let audio_lock = manager
+            .join(guild_id, channel)
+            .map(|handler| {
                 // Sometimes, things can move a little quickly.
                 // The sound might play while the connected "ping" is still playing.
                 // Sleep for just a little bit before playing.
                 thread::sleep(Duration::from_secs(1));
                 handler.play_only(audio_source)
-            }
-            None => {
-                eprintln!("Unable to get a handler for the voice channel.");
-                return;
-            }
-        };
+            })
+            .unwrap();
 
         // Poll until we're done.
         thread::spawn(move || {
@@ -98,13 +68,7 @@ impl EventHandler for Handler {
                 }
                 thread::sleep(Duration::from_millis(500));
             }
-            let vm = match get_voice_manager_from_cache(&ctx) {
-                Some(vm) => vm,
-                None => {
-                    eprintln!("No voice manager in cache.");
-                    return;
-                }
-            };
+            let vm = get_voice_manager_from_cache(&ctx).unwrap();
             vm.lock().leave(guild_id);
             // This currently leaves a zombie, which is something we should address...
         });
@@ -112,13 +76,10 @@ impl EventHandler for Handler {
 
     fn ready(&self, ctx: Context, payload: Ready) {
         println!("[{}] connected to Discord.", payload.user.name);
-        let url = match payload.user.invite_url(&ctx.http, Permissions::empty()) {
-            Ok(v) => v,
-            Err(why) => {
-                eprintln!("Error getting invite url: {:?}", why);
-                return;
-            }
-        };
+        let url = payload
+            .user
+            .invite_url(&ctx.http, Permissions::empty())
+            .unwrap();
 
         println!("Invite URL: {}", url);
     }
@@ -137,26 +98,13 @@ fn get_voice_manager_from_cache(ctx: &Context) -> Option<Arc<Mutex<ClientVoiceMa
 }
 
 fn upload_sound(ctx: &Context, msg: &Message, source: &PathBuf) {
-    let file = match File::open(source) {
-        Ok(file) => file,
-        Err(why) => {
-            eprintln!("Error opening file: {:?}", why);
-            return;
-        }
-    };
-
+    let file = File::open(source).unwrap();
     let name = source.file_name().unwrap().to_str().unwrap();
 
-    let result = msg
-        .channel_id
+    msg.channel_id
         .send_files(&ctx.http, vec![(&file, name)], |m| {
             m.content("You aren't in a channel, but here's the sound...");
             m
-        });
-    match result {
-        Ok(_a) => {}
-        Err(why) => {
-            eprintln!("Unable to upload sound: {:?}", why);
-        }
-    };
+        })
+        .ok();
 }

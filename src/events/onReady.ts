799ef7b12a237aefa5060e610d3269c2e8c3c513
Ben Christianson <thhuntertgm@gmail.com>
2020-12-02 19:26:28 -0600

    Add !help notifier

--- src/events/onReady.ts
+++ src/events/onReady.ts
@@ -4,4 +4,5 @@ export default (client: Client) => (): void => {
   void client.generateInvite().then((url) => {
     console.log(`Invite URL: ${url}`);
   });
+  void client.user?.setActivity('!help for commands');
 };

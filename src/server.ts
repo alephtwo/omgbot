d3537808f33a84a0b38b8e8058f001de472b3915
Ben Christianson <thhuntertgm@gmail.com>
2021-07-06 23:36:46 -0500

    Upgrade discordjs

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,11 @@ import onMessage from './events/onMessage';
 import onReady from './events/onReady';
 
 const client = new Discord.Client({
-  intents: Discord.Intents.NON_PRIVILEGED,
+  intents: [
+    Discord.Intents.FLAGS.GUILDS,
+    Discord.Intents.FLAGS.GUILD_MESSAGES,
+    Discord.Intents.FLAGS.GUILD_VOICE_STATES,
+  ]
 });
 
 client.on('ready', onReady(client));

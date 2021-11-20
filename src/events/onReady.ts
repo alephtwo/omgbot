d2af4d048968c6fec2e977469b0199d2518f99c5
Ben Christianson <thhuntertgm@gmail.com>
2021-11-19 20:20:30 -0600

    make it just a bot

--- src/events/onReady.ts
+++ src/events/onReady.ts
@@ -2,8 +2,7 @@ import { Client } from 'discord.js';
 
 export default (client: Client) => (): void => {
   const url = client.generateInvite({
-    // TODO: this should just be a bot, but it won't compile like that
-    scopes: ['applications.commands'],
+    scopes: ['bot'],
   });
   console.log(`Invite URL: ${url}`);
 

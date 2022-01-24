5c8fa05b3a1412072a208ef0f9f375dafb26e024
Ben Christianson <thhuntertgm@gmail.com>
2022-01-23 21:25:04 -0600

    Properly glob only files, not dirs

--- src/sound/soundUtils.ts
+++ src/sound/soundUtils.ts
@@ -4,13 +4,13 @@ import * as path from 'path';
 const root = path.join(__dirname, '..', '..', 'sounds');
 
 export function pickRandomSound(): string {
-  const sounds = glob.sync(path.join(root, '**', '*'));
+  const sounds = globFiles(path.join(root, '**', '*'));
   const index = Math.floor(Math.random() * sounds.length);
   return sounds[index];
 }
 
 export function pickSound(category: string): string {
-  const sounds = glob.sync(path.join(root, category, '**', '*'));
+  const sounds = globFiles(path.join(root, category, '**', '*'));
   const index = Math.floor(Math.random() * sounds.length);
   return sounds[index];
 }
@@ -18,4 +18,8 @@ export function pickSound(category: string): string {
 export function getAllCategories(): Set<string> {
   const dirs = glob.sync(path.join(root, '*')).map((f) => path.basename(f));
   return new Set(dirs);
+}
+
+function globFiles(globstring: string) {
+  return glob.sync(globstring, { nodir: true });
 }

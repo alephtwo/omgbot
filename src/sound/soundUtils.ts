1e634a56825f38a3d8c768d30387db1983898cde
Ben Christianson <thhuntertgm@gmail.com>
2024-05-24 21:08:41 -0500

    Add stats command

--- src/sound/soundUtils.ts
+++ src/sound/soundUtils.ts
@@ -2,20 +2,20 @@ import { globSync } from "glob";
 import * as path from "path";
 import sample from "../random/sample";
 
-const root = path.join(__dirname, "..", "..", "sounds");
+export const SOUND_DIR = path.join(__dirname, "..", "..", "sounds");
 
 export function pickRandomSound(): string {
-  const sounds = globFiles(path.join(root, "**", "*"));
+  const sounds = globFiles(path.join(SOUND_DIR, "**", "*"));
   return sample(sounds);
 }
 
 export function pickSound(category: string): string {
-  const sounds = globFiles(path.join(root, category, "**", "*"));
+  const sounds = globFiles(path.join(SOUND_DIR, category, "**", "*"));
   return sample(sounds);
 }
 
 export function getAllCategories(): Set<string> {
-  const dirs = globSync(path.join(root, "*")).map((f) => path.basename(f));
+  const dirs = globSync(path.join(SOUND_DIR, "*")).map((f) => path.basename(f));
   return new Set(dirs);
 }
 
